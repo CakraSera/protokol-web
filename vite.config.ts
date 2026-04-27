002841d21e89cf631f262505336d1a26794b7ebf
CakraSera <rakelsera2001@gmail.com>
2026-04-28 06:24:57 +0700

    chore: setup vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
